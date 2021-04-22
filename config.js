9d5ee66939bde1e5a75e2d871ee41cae952700f3
Stackbit Code Editor <projects@stackbit.com>
2021-04-22 17:05:33 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663398',
+  manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
